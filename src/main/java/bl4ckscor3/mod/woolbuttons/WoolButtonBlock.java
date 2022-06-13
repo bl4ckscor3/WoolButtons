ec82aaf961050518e8ad2bd39a1dc4842bff562b
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-06-13 23:20:54 +0200

    update to 1.19

--- src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtonBlock.java
+++ src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtonBlock.java
@@ -1,17 +1,14 @@
 package bl4ckscor3.mod.woolbuttons;
 
-import net.minecraft.world.level.block.ButtonBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.block.ButtonBlock;
 
 public class WoolButtonBlock extends ButtonBlock
 {
-	public WoolButtonBlock()
+	public WoolButtonBlock(Properties properties)
 	{
-		super(true, Block.Properties.of(Material.DECORATION).noCollission().strength(0.5F).sound(SoundType.WOOL));
+		super(true, properties);
 	}
 
 	@Override
@@ -19,6 +16,7 @@ public class WoolButtonBlock extends ButtonBlock
 	{
 		if(Configuration.CONFIG.makeSound.get())
 			return isPressed ? SoundEvents.WOODEN_BUTTON_CLICK_ON : SoundEvents.WOODEN_BUTTON_CLICK_OFF;
-		return WoolButtons.silence; //can't be null, would crash
+
+		return WoolButtons.SILENCE.get(); //can't be null, would crash
 	}
 }
--- src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
+++ src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
@@ -1,64 +1,63 @@
 package bl4ckscor3.mod.woolbuttons;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries.Keys;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(WoolButtons.MODID)
 @EventBusSubscriber(modid=WoolButtons.MODID, bus=Bus.MOD)
 public class WoolButtons
 {
 	public static final String MODID = "sbmwoolbuttons";
-	@ObjectHolder(MODID + ":silence")
-	public static SoundEvent silence;
+	public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
+	public static final RegistryObject<SoundEvent> SILENCE = SOUND_EVENTS.register("silence", () -> new SoundEvent(new ResourceLocation(MODID, "silence")));
 
 	public WoolButtons()
 	{
+		SOUND_EVENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
 		if(FMLLoader.getDist() == Dist.CLIENT)
 			ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Configuration.CONFIG_SPEC);
 	}
 
 	@SubscribeEvent
-	public static void registerBlocks(RegistryEvent.Register<Block> event)
+	public static void onRegister(RegisterEvent event)
 	{
-		for(Color color : Color.values())
-		{
-			event.getRegistry().register(new WoolButtonBlock().setRegistryName(getName(color)));
-		}
-	}
+		event.register(Keys.BLOCKS, helper -> {
+			for(Color color : Color.values())
+			{
+				helper.register(getName(color), new WoolButtonBlock(Block.Properties.of(Material.DECORATION).noCollission().strength(0.5F).sound(SoundType.WOOL)));
+			}
+		});
+		event.register(Keys.ITEMS, helper -> {
+			for(Color color : Color.values())
+			{
+				ResourceLocation name = getName(color);
+				Block block = ForgeRegistries.BLOCKS.getValue(name);
 
-	@SubscribeEvent
-	public static void registerItems(RegistryEvent.Register<Item> event)
-	{
-		for(Color color : Color.values())
-		{
-			Block block = ForgeRegistries.BLOCKS.getValue(getName(color));
-
-			if(block != null)
-				event.getRegistry().register(new BlockItem(block, new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)).setRegistryName(block.getRegistryName()));
-		}
-	}
-
-	@SubscribeEvent
-	public static void registerSounds(RegistryEvent.Register<SoundEvent> event)
-	{
-		ResourceLocation rl = new ResourceLocation(MODID, "silence");
-
-		event.getRegistry().register(new SoundEvent(rl).setRegistryName(rl));
+				if(block != null)
+					helper.register(name, new BlockItem(block, new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+			}
+		});
 	}
 
 	public static ResourceLocation getName(Color color)
