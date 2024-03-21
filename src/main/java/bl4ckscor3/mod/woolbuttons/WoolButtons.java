1f44fd19b56cb4d8455f8047feeeb416869e6c74
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-03-21 17:24:44 +0100

    make sculk sensors ignore wool buttons

--- src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
+++ src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
@@ -78,7 +78,7 @@ public class WoolButtons {
 		return new ResourceLocation(MODID, "wool_button_" + color.name().toLowerCase());
 	}
 
-	private static enum Color {
+	public enum Color {
 		WHITE,
 		LIGHT_GRAY,
 		GRAY,
