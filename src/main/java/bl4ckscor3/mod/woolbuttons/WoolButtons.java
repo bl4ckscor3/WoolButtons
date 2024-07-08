714f46a95afecd2b53ba828f3035d3ad2c22943f
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-07-08 16:27:21 +0200

    fix crash on newer NeoForge versions

--- src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
+++ src/main/java/bl4ckscor3/mod/woolbuttons/WoolButtons.java
@@ -67,7 +67,7 @@ public class WoolButtons {
 	@SubscribeEvent
 	public static void onCreativeModeTabBuildContents(BuildCreativeModeTabContentsEvent event) {
 		if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS)
-			event.getEntries().putAfter(new ItemStack(Items.STONE_BUTTON), new ItemStack(ITEMS.get(Color.WHITE)), TabVisibility.PARENT_AND_SEARCH_TABS);
+			event.insertAfter(new ItemStack(Items.STONE_BUTTON), new ItemStack(ITEMS.get(Color.WHITE)), TabVisibility.PARENT_AND_SEARCH_TABS);
 		else if (event.getTabKey() == CreativeModeTabs.COLORED_BLOCKS)
 			event.acceptAll(ITEMS.values().stream().map(ItemStack::new).toList());
 	}
